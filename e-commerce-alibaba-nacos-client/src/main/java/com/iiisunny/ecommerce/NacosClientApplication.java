bbd0de4f0e153ee1bfe9ff8551dbabfd7781d6f6
iiisunny <4487071@qq.com>
2022-07-19 09:11:23 +0800

    feat: 微服务通信集成Ribbon + OpenFegin

--- e-commerce-alibaba-nacos-client/src/main/java/com/iiisunny/ecommerce/NacosClientApplication.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/iiisunny/ecommerce/NacosClientApplication.java
@@ -3,10 +3,14 @@ package com.iiisunny.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * <h1>Nacos Client 工程启动入口</h1>
  */
+@EnableFeignClients
+@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosClientApplication {
